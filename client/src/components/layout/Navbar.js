cbdc9501a0eff8a6c4975630fc8deca08208f9f2
Michael Bentley <michaelbentley95@gmail.com>
2019-03-18 23:15:30 -0400

    Added profile creation

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -16,7 +18,12 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a onClick={this.onLogoutClick.bind(this)} className="nav-link">
+          <div
+            role="menuitem"
+            onClick={this.onLogoutClick.bind(this)}
+            className="nav-link"
+            style={{ cursor: "pointer" }}
+          >
             <img
               className="rounded-circle"
               src={user.avatar}
@@ -25,7 +32,7 @@ class Navbar extends Component {
               title="You must have a Gravatar connected to your email to display an image"
             />
             Logout
-          </a>
+          </div>
         </li>
       </ul>
     );
@@ -88,5 +95,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
