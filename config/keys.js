0e1978e29ebf51693f46fe9f48e08b9e18cb4b66
Phillip Bentley <michael.bentley@siemens.com>
2019-03-19 16:59:58 -0400

    Added buttons for Dashboard

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
   mongoURI:
+  	"mongodb://localhost:27017/myapp",
+  mongoURI2:
     "mongodb+srv://michaelb_asinus:Thingaboutit%21@cluster0-3gyly.azure.mongodb.net/test?retryWrites=true",
   secretOrKey: "bigchungus"
 };
