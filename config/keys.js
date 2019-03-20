274a320b8e75006edd7798bebc3db9b013ceaf91
Michael Bentley <michaelbentley95@gmail.com>
2019-03-19 22:31:57 -0400

    Implemented Experience and Education

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 module.exports = {
+  mongoURI2: "mongodb://localhost:27017/myapp",
   mongoURI:
-  	"mongodb://localhost:27017/myapp",
-  mongoURI2:
     "mongodb+srv://michaelb_asinus:Thingaboutit%21@cluster0-3gyly.azure.mongodb.net/test?retryWrites=true",
   secretOrKey: "bigchungus"
 };
