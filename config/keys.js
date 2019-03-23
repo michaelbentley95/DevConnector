48896affb72a247697e80c538cebf9133df0f9cb
Michael Bentley <michaelbentley95@gmail.com>
2019-03-22 23:24:27 -0400

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI2: "mongodb://localhost:27017/myapp",
-  mongoURI:
-    "mongodb+srv://michaelb_asinus:Thingaboutit%21@cluster0-3gyly.azure.mongodb.net/test?retryWrites=true",
-  secretOrKey: "bigchungus"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI2: "mongodb://localhost:27017/myapp",
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+};
